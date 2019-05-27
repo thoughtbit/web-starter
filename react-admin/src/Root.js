c5a6d15fae62a1a357a886c891f8fcec94fd944e
moocss <moocss>
2019-05-27 17:24:18 +0800

    初始化

--- react-admin/src/Root.js
+++ react-admin/src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { Router, history } from './router'
+import { Router, history } from './router';
 
 const Root = ({ store, routeConfig }) => {
   return (
