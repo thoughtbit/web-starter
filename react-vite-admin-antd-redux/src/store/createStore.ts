eec339a91a59a89de08e8e5e9b112dbd5355d5d0
moocss <moocss@163.com>
2022-08-15 10:45:24 +0800

    add: 扩展createStore

--- react-vite-admin-antd-redux/src/store/createStore.ts
+++ react-vite-admin-antd-redux/src/store/createStore.ts
@@ -4,22 +4,31 @@ import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist
 import { DEBUG } from "~/constants";
 import { api as ApiService } from "~/services";
 
-export default (rootReducer: Reducer, middlewares: Middleware[], listener: Middleware) => {
-  const store = configureStore({
+export default (
+  rootReducer: Reducer,
+  middlewares?: Middleware[],
+  listener?: Middleware,
+  initialState?: Partial<unknown>
+) =>
+  configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
+    middleware: (getDefaultMiddleware) => {
+      const defaultMiddleware = getDefaultMiddleware({
         thunk: { extraArgument: ApiService },
         immutableCheck: false,
         // serializableCheck: false,
         serializableCheck: {
           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
         },
-      })
-        .concat(middlewares)
-        .prepend(listener),
+      });
+
+      middlewares && defaultMiddleware.concat(middlewares);
+      listener && defaultMiddleware.prepend(listener);
+
+      return defaultMiddleware;
+    },
+    preloadedState: {
+      ...initialState,
+    },
     devTools: DEBUG,
   });
-
-  return store;
-};
